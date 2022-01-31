a310583f9c9e90a8d9153144cabf56d2033ff2a3
Sittidet Pawutinan <stdpwtnjj.2544@gmail.com>
2022-02-01 00:28:46 +0700

    create MovingPlatform

---
+++ Assets/Scripts/Platforms/MovingPlatform.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] int startPoint;
+    [SerializeField] float stopMoveDuration;
+    [SerializeField] Transform[] points;
+
+    private int index;
+    private float stopTime;
+    private bool isMoving;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = points[startPoint].transform.position;
+        index = startPoint++;
+        isMoving = true;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isMoving)
+        {
+            if (Vector2.Distance(transform.position, points[index].transform.position) < 0.01f)
+            {
+                index++;
+                if (index == points.Length)
+                {
+                    index = 0;
+                }
+                stopTime = Time.time;
+                isMoving = false;
+            }
+            else
+            {
+                transform.position = Vector2.MoveTowards(transform.position, points[index].transform.position, speed * Time.deltaTime);
+            }
+        }else{
+            if(Time.time > stopMoveDuration + stopTime){
+                isMoving = true;
+            }
+        }
+
+    }
+}
