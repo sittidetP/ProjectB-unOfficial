1e9e70e077731b944630a6076cfe6567435acd0e
Sittidet Pawutinan <stdpwtnjj.2544@gmail.com>
2022-02-05 01:35:36 +0700

    create Boundaries.cs

---
+++ Assets/Scripts/Boundaries.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boundaries : MonoBehaviour
+{
+    [SerializeField] GameObject[] objects;
+
+    private Player player;
+
+    private void Start() {
+        setActiveOjects(false);
+    }
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.gameObject.CompareTag("Player")){
+            setActiveOjects(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        if(other.gameObject.CompareTag("Player")){
+            setActiveOjects(false);
+        }
+    }
+
+    private void setActiveOjects(bool value){
+        foreach(GameObject obj in objects){
+            obj.gameObject.SetActive(value);
+        }
+    }
+}
