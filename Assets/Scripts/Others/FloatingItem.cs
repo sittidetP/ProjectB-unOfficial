1c6249d18a52f9b86a38d5453ad55d9454115074
sittidetP <pawutinan_s@silpakorn.edu>
2022-02-20 20:24:43 +0700

    create FloatingItem

---
+++ Assets/Scripts/Others/FloatingItem.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatingItem : MonoBehaviour
+{
+    [SerializeField] float swingRadius;
+
+    float startTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startTime = Time.realtimeSinceStartup;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float currentTimePass = Time.realtimeSinceStartup - startTime;
+        float newY = swingRadius * Mathf.Sin(currentTimePass);
+
+        transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+    }
+}
