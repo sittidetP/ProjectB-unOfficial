ae5f806abee75f6b5eae3714e7e9ea02d9348bb0
sittidetP <pawutinan_s@silpakorn.edu>
2022-02-21 22:54:10 +0700

    making StartScene (2)

--- Assets/Scripts/UI/UIStartSceneManager.cs
+++ Assets/Scripts/UI/UIStartSceneManager.cs
@@ -1,18 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UIStartSceneManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] string startScene;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void StartButton(){
+        SceneManager.LoadScene(startScene);
+    }
+    public void ExitButton(){
+        Application.Quit();
     }
 }
