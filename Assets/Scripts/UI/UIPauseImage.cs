9e2c115ec1a922b5a13fecf89043c60a11ae7f3b
sittidetP <pawutinan_s@silpakorn.edu>
2022-02-21 20:34:36 +0700

    make Pause Screen to Quit to Tile

--- Assets/Scripts/UI/UIPauseImage.cs
+++ Assets/Scripts/UI/UIPauseImage.cs
@@ -1,14 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UIPauseImage : MonoBehaviour
 {
+    private string startScene = "StartScene";
     public void ResumeButton()
     {
         if (PauseManager.isPause)
         {
             PauseManager.resume();
         }
+    }
+
+    public void QuitButton(){
+        SceneManager.LoadScene(startScene);
     }
 }
