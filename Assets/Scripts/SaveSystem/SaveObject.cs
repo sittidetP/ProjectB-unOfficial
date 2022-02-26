89441f719495b8ce6aa1079f4fe9fbdaa406e882
sittidetP <pawutinan_s@silpakorn.edu>
2022-02-26 13:00:19 +0700

    making Pause when Save (4)

---
+++ Assets/Scripts/SaveSystem/SaveObject.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveObject
+{
+    Player player;
+
+    SaveObject(Player player){
+        this.player = player;
+    }
+}
