1f1b72a172fc28d12fdec4a29f63347ea938e508
sittidetP <pawutinan_s@silpakorn.edu>
2022-03-01 19:20:51 +0700

    add canCollect In Potion

--- Assets/Scripts/Player/Collectable/Collectable.cs
+++ Assets/Scripts/Player/Collectable/Collectable.cs
@@ -5,15 +5,23 @@ using UnityEngine;
 public abstract class Collectable : MonoBehaviour
 {
     protected Player player;
+    
     private void OnTriggerEnter2D(Collider2D other) {
         player = other.GetComponent<Player>();
         if(player != null){
-            ActionItem();
-            Destroy(gameObject);
+            if(canCollect){
+                ActionItem();
+                Destroy(gameObject);
+            }
+            
         }
     }
 
     protected virtual void ActionItem(){
         
+    }
+
+    protected virtual bool canCollect{
+        get => true;
     }
 }
