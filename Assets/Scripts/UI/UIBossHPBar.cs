95045018399c00e19d000f86fe069dfbca9db665
sittidetP <pawutinan_s@silpakorn.edu>
2022-03-06 00:38:28 +0700

    finish Boss1(BigSlime)wUI

--- Assets/Scripts/UI/UIBossHPBar.cs
+++ Assets/Scripts/UI/UIBossHPBar.cs
@@ -7,4 +7,19 @@ public class UIBossHPBar : MonoBehaviour
 {
     [SerializeField] Enemy boss;
     [SerializeField] Image maskHPBar;
+    private float originalMaskHpBarSize;
+
+    private void Start() {
+        originalMaskHpBarSize = maskHPBar.rectTransform.rect.width;
+    }
+
+    private void Update() {
+        if(boss != null){
+            SetHPBarValue(boss.Core.Stats.getHPRatio());
+        }
+    }
+
+    private void SetHPBarValue(float value){
+        maskHPBar.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, originalMaskHpBarSize * value);
+    }
 }
