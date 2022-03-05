8897d68c7fb5ed9cc5af90231b9b285b7f0a78d1
sittidetP <pawutinan_s@silpakorn.edu>
2022-03-06 00:28:33 +0700

    making UIBoss(2)

---
+++ Assets/Scripts/UI/UIBossHPBar.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIBossHPBar : MonoBehaviour
+{
+    [SerializeField] Enemy boss;
+    [SerializeField] Image maskHPBar;
+}
