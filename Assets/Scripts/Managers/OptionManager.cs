7dcca38ceab1d59062ef37e4185edf6b9b33a3ca
sittidetP <pawutinan_s@silpakorn.edu>
2022-03-08 13:30:37 +0700

    making OptionManager and OptionButton

---
+++ Assets/Scripts/Managers/OptionManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class OptionManager : MonoBehaviour
+{
+    [SerializeField] AudioMixer mixer;
+    [SerializeField] Slider sliderBGM;
+    [SerializeField] Slider sliderSFX;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
