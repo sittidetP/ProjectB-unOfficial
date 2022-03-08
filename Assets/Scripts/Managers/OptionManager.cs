978334877f135c3b0cd60c237afeb459192ef9de
sittidetP <pawutinan_s@silpakorn.edu>
2022-03-08 14:17:36 +0700

    finish Options setting part

--- Assets/Scripts/Managers/OptionManager.cs
+++ Assets/Scripts/Managers/OptionManager.cs
@@ -9,6 +9,8 @@ public class OptionManager : MonoBehaviour
     [SerializeField] AudioMixer mixer;
     [SerializeField] Slider sliderBGM;
     [SerializeField] Slider sliderSFX;
+    private float bgmVol = 0;
+    private float seVol = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,13 +25,19 @@ public class OptionManager : MonoBehaviour
 
     public void SetBGM(float vol){
         if(mixer != null){
-
+            mixer.SetFloat("BGM_VOL", vol);
+            bgmVol = vol;
+            sliderBGM.value = vol;
         }
     }
 
     public void SetSE(float vol){
         if(mixer != null){
-            
+            mixer.SetFloat("SE_VOL", vol);
+            mixer.SetFloat("SE_E_VOL", vol);
+            mixer.SetFloat("SE_P_VOL", vol);
+            seVol = vol;
+            sliderSFX.value = vol;
         }
     }
 }
