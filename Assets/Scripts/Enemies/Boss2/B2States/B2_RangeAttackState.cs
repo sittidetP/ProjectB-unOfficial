e05c67b21a363d224d33a8fb34a36b690a1a0fdf
sittidetP <pawutinan_s@silpakorn.edu>
2022-03-17 23:38:23 +0700

    making Boss2 (7.5)

---
+++ Assets/Scripts/Enemies/Boss2/B2States/B2_RangeAttackState.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class B2_RangeAttackState : BaseRangeAttackState
+{
+    Boss2 boss2;
+    public B2_RangeAttackState(Entity entity, FiniteStateMachine stateMachine, string animBoolName, BaseArgoStateData stateData, Transform enemyEye, Transform attackPosition, BaseRangeAttackStateData rangeAttackStateData, Transform rangeAttackPosition, Boss2 boss2) : base(entity, stateMachine, animBoolName, stateData, enemyEye, attackPosition, rangeAttackStateData, rangeAttackPosition)
+    {
+        this.boss2 = boss2;
+    }
+
+    public override void LogicUpdate()
+    {
+        base.LogicUpdate();
+
+        if(!canAttack){
+            stateMachine.ChangeState(boss2.MoveState);
+        }
+    }
+}
--- Assets/Scripts/Enemies/Boss2/Boss2.cs
+++ Assets/Scripts/Enemies/Boss2/Boss2.cs
@@ -7,17 +7,20 @@ public class Boss2 : Enemy
     public B2_IdleState IdleState {get; private set;}
     public B2_MoveState MoveState {get; private set;}
     public B2_MeleeAttackState MeleeAttackState {get; private set;}
+    public B2_RangeAttackState RangeAttackState {get; private set;}
     public B2_DeadState DeadState {get; private set;}
     [Header("States Data")]
     [SerializeField] BaseArgoStateData argoStateData;
     [SerializeField] BaseIdleStateData idleStateData;
     [SerializeField] BaseMoveStateData moveStateData;
     [SerializeField] BaseMeleeAttackStateData meleeAttackStateData;
+    [SerializeField] BaseRangeAttackStateData rangeAttackStateData;
     [SerializeField] BaseHurtStateData hurtStateData;
 
     [Header("Other Objects")]
     [SerializeField] Transform enemyEye;
     [SerializeField] Transform meleeHitboxPosition;
+    [SerializeField] Transform rangeAttackPosition;
     [SerializeField] int debugFacing = 1;
     [SerializeField] AudioClip deadSFX;
     public AudioClip DeadSFX { get => deadSFX; }
@@ -33,6 +36,7 @@ public class Boss2 : Enemy
         IdleState = new B2_IdleState(this, StateMachine, "idle", argoStateData, enemyEye, idleStateData, this);
         MoveState = new B2_MoveState(this, StateMachine, "move", argoStateData, enemyEye, moveStateData, this);
         MeleeAttackState = new B2_MeleeAttackState(this, StateMachine, "attack", argoStateData, enemyEye, meleeHitboxPosition, meleeAttackStateData, this);
+        RangeAttackState = new B2_RangeAttackState(this, StateMachine, "cast", argoStateData, enemyEye, meleeHitboxPosition, rangeAttackStateData, rangeAttackPosition, this);
         DeadState = new B2_DeadState(this, StateMachine, "dead", itemDroper, this);
         /*
         MoveState = new B3_MoveState(this, StateMachine, "move", argoStateData, enemyEye, moveStateData, this);
